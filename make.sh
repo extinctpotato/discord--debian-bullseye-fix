57cd9ce3f4c56517f3acd875c64fc630e829eeb8
Adam Olech <nddr89@gmail.com>
2022-02-20 13:32:22 +0000

    Add make.sh

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+discord_version="0.0.16"
+old_deb="1.deb"
+new_deb="2.deb"
+unpack_location="$old_deb-unpack"
+
+clean() {
+    rm -rf $old_deb || true
+    rm -rf $new_deb || true
+    rm -rf $unpack_location || true
+}
+
+clean
+
+wget -O $old_deb https://dl.discordapp.net/apps/linux/$discord_version/discord-$discord_version.deb
+
+dpkg-deb -x $old_deb $unpack_location
+dpkg-deb --control $old_deb
+
+mv DEBIAN $unpack_location
+cp control $unpack_location/DEBIAN/control
+
+dpkg -b $unpack_location $new_deb
